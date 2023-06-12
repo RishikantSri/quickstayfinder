2f47ad8c450fcdf5b7c65bc28ee131907a5be5e7
RishikantSri <>
2023-06-12 12:38:29 +0530

    Registration API with validation messages created;

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Rule;
+
+class RegisterController extends Controller
+{
+    //
+    public function __invoke(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+            'role_id' => ['required', Rule::in(Role::ROLE_OWNER, Role::ROLE_USER)],
+        ]);
+
+   
+        if ($validator->fails()) {
+            $response = [
+                'code' => 200,
+                'status' => false,
+                'message' => $validator->errors()->first(),
+            ];
+            return response()->json($response, 200);
+        }
+
+
+        // some laravel error work on this later   
+
+        // $request->validate([
+        //     'name' => ['required', 'string', 'max:255'],
+        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        //     'password' => ['required', 'confirmed', Password::defaults()],
+        //     'role_id' => ['required', Rule::in(Role::ROLE_OWNER, Role::ROLE_USER)],
+        // ]);
+
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role_id' => $request->role_id,
+        ]);
+
+        return response()->json([
+            'access_token' => $user->createToken('client')->plainTextToken,
+        ]);
+    }
+}
